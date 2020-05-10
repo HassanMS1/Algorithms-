919188f28e6b74d1da8b40eb704fe7345826cda6
HassanMS1 <60139823+HassanMS1@users.noreply.github.com>
2020-05-10 12:09:09 +0200

    initial commit

---
+++ src/Bitwise.java
@@ -0,0 +1,7 @@
+public class Bitwise {
+    public static void main(String[] args) {
+        System.out.println(8 & 5 ); // adds the binary sequence in truth table 1 + 0 = 0 , 1 + 1 = 1
+        System.out.println( 2 | 3 ); // 1 or 0  = 1
+        System.out.println(~1); // flips a 32 bit signed (two complent) number : 1111 11111 1111 1111
+    }
+}
